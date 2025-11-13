4fda65f0dc303f8d7b190ca4e1f28ae38ad7cc0a
jangse0 <syjang1016@korea.ac.kr>
2025-11-13 10:24:18 +0900

    Initial commit

---
+++ src/main/java/com/example/newplanner/NewPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.newplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newplanner/NewPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
